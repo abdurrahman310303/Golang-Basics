0a3a1b36fcd1602768e55ef8c94e8b2eb6891a62
abdurrahman310303 <abdurrahman310303@gmail.com>
2025-09-08 14:39:24 +0500

    switch statement

---
+++ switch-selection.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("Switch Selection Structure")
+
+	rand.Seed(time.Now().UnixNano())
+	diceNumber := rand.Intn(6) + 1
+
+	fmt.Println("value of dice is", diceNumber)
+
+	switch diceNumber {
+	case 1:
+		fmt.Println("You can move 1 spot")
+	case 2:
+		fmt.Println("You can move 2 spots")
+	case 3:
+		fmt.Println("You can move 3 spots")
+		fallthrough
+	case 4:
+		fmt.Println("You can move 4 spots")
+	case 5:
+		fmt.Println("You can move 5 spots")
+
+	case 6:
+		fmt.Println("You can move 6 spots and roll the dice again")
+	default:
+		fmt.Println("what was that?")
+	}
+}
